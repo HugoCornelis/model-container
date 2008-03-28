376d34829caa3037c44f8cc2c4d27a8544ada0eb
Hugo Cornelis <hugo.cornelis@gmail.com>
2008-03-28 20:17:39 +0000

    1. Keywords only, fixing consistency.

--- neurospaces.c
+++ neurospaces.c
@@ -1712,7 +1712,7 @@ static int NeurospacesStartTimingReport(void)
 char * NeurospacesGetVersion(void)
 {
     // $Format: "    static char *pcVersion=\"${package}-${label}\";"$
-    static char *pcVersion="model-container-build-12";
+    static char *pcVersion="model-container-build-11";
 
     return(pcVersion);
 }
