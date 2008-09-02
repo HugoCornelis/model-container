ad7aed87d8ceefb8edf85010693d33353db6b5cd
Hugo Cornelis <hugo.cornelis@gmail.com>
2008-09-02 19:02:28 +0000

    1. Keywords only: network-16

--- neurospaces.c
+++ neurospaces.c
@@ -1715,7 +1715,7 @@ static int NeurospacesStartTimingReport(void)
 char * NeurospacesGetVersion(void)
 {
     // $Format: "    static char *pcVersion=\"${package}-${label}\";"$
-    static char *pcVersion="model-container-network-15";
+    static char *pcVersion="model-container-network-16";
 
     return(pcVersion);
 }
