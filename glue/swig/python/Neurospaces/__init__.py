af692d24c323a9deb45657d2baeb939811b64618
mandorodriguez <mandorodriguez@gmail.com>
2010-04-15 23:05:45 +0000

    1. Added a path append to the python components.

--- glue/swig/python/Neurospaces/__init__.py
+++ glue/swig/python/Neurospaces/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import sys
+sys.path.append('/usr/local/glue/swig/python')
 import SwiggableNeurospaces
 
 nmcGlobal = None
---
+++ glue/swig/python/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('/usr/local/glue/swig/python')
