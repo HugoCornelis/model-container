c15ee1c60e89cc1228e6a9062d3eb4a7524744fa
mandorodriguez <mandorodriguez@gmail.com>
2010-12-03 20:05:50 +0000

    1. Added a stub for what will become the nmc python module for the model container.

---
+++ glue/swig/python/nmc/__init__.py
@@ -0,0 +1,16 @@
+"""!
+The nmc pythom module is a Pythonic abstraction over the
+Neurospaces Model Container. It's purpose is to allow
+scripting functionality to the Model Container via Python,
+while providing a usuable API that does not require knowledge
+of each data structure that the Model Container employs.
+
+"""
+
+
+__author__ = 'Mando Rodriguez'
+__credits__ = []
+__license__ = "GPL"
+__version__ = "0.1"
+__status__ = "Development"
+
