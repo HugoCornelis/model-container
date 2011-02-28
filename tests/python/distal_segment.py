2433626513c10c13a7acbfa477c3123b5d76e574
mandorodriguez <mandorodriguez@gmail.com>
2011-02-28 22:31:06 +0000

    1. Created a test script to load a purkinje cell and uses the query function for obtaining the names of distial segments of each dendrite, as shown in the cbi-scripting paper.

---
+++ tests/python/distal_segment.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+"""
+Basic test to check the segment data type.
+"""
+import os
+import pdb
+import sys
+
+root_path = os.environ['HOME'] + "/neurospaces_project/model-container/source/snapshots/0"
+
+my_path = root_path + "/glue/swig/python/"
+
+sys.path.append(my_path)
+
+import nmc
+
+my_nmc = nmc.ModelContainer(None)
+
+
+my_nmc.Read("cells/purkinje/edsjb1994.ndf")
+
+my_nmc.Query("segmentertips /Purkinje")
+
+print "Done!"
+
