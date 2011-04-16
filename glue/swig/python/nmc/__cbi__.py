44a4003ba0a3f1571968eb718a21dcaab0dc3b5d
mandorodriguez+MacPro <mandorodriguez+MacPro@gmail.com>
2011-04-16 22:20:01 +0000

    1. Updated release expand config to remove the newline character for the monotone revision hash.
    2. Fixed formatting in __cbi__ identifier to output tokens as strings.

--- glue/swig/python/nmc/__cbi__.py
+++ glue/swig/python/nmc/__cbi__.py
@@ -6,17 +6,16 @@ into the CBI architecture.
 """
 
 def GetRevisionInfo():
-# $Format: "    return ${monotone_id}"$
-    return b11538118c7faa37b99d429dfd8c032dc5f36632
-
+# $Format: "    return \"${monotone_id}\""$
+    return "f6df32a8fae6f885ae010bf3272e85f6aa073b45"
 
 def GetPackageName():
-# $Format: "    return ${package}"$
-    return model-container
+# $Format: "    return \"${package}\""$
+    return "model-container"
 
 def GetVersion():
-# $Format: "    return ${major}.${minor}.${micro}-${label}"$
-    return 0.0.0-alpha
+# $Format: "    return \"${major}.${minor}.${micro}-${label}\""$
+    return "0.0.0-alpha"
 
 def GetDependencies():
     """!
