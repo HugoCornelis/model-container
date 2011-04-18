eb7a19b068a6a8a2c39bc7b605c55adbd1155505
mandorodriguez+MacPro <mandorodriguez+MacPro@gmail.com>
2011-04-18 03:53:31 +0000

    1. Added a script for python disttools.

--- glue/swig/python/nmc/__cbi__.py
+++ glue/swig/python/nmc/__cbi__.py
@@ -5,6 +5,17 @@ This file provides data for a packages integration
 into the CBI architecture.
 """
 
+__author__ = "Mando Rodriguez"
+__copyright__ = "Copyright 2010, The GENESIS Project"
+__credits__ = ["Hugo Cornelis", "Dave Beeman"]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Mando Rodriguez"
+__email__ = "rodrigueza14@uthscsa.edu"
+__status__ = "Development"
+__url__ = "http://genesis-sim.org"
+__download_url__ = ""
+
 def GetRevisionInfo():
 # $Format: "    return \"${monotone_id}\""$
     return "f6df32a8fae6f885ae010bf3272e85f6aa073b45"
