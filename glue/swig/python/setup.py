0d433399243a7783b1719e932654e4459438b645
mandorodriguez <mandorodriguez@gmail.com>
2011-04-21 19:43:45 +0000

    1. Updated mtn-ignore file to ignore python build files.
    2. Added clean targets for removing python build files.
    
    3. Updated python setup script for the nmc module.

--- glue/swig/python/setup.py
+++ glue/swig/python/setup.py
@@ -1,21 +1,102 @@
 import os
+import pdb
+import sys
 from distutils.core import setup
-
+from distutils.command.install_data import install_data
 import nmc.__cbi__ as cbi
 
+
+#-------------------------------------------------------------------------------
+
+#
+# This is borrowed from django's setup tools
+# taken from here http://code.djangoproject.com/browser/django/trunk/setup.py
+#
+class osx_install_data(install_data):
+    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
+    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
+    # for this in distutils.command.install_data#306. It fixes install_lib but not
+    # install_data, which is why we roll our own install_data class.
+	
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to the
+        # fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+
+#-------------------------------------------------------------------------------
+
+"""
+Function for reading in a file and outputting it as a string. 
+"""
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+#-------------------------------------------------------------------------------
+
+
+# This is borrowed from django's setup tools
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+#-------------------------------------------------------------------------------
+
+
+"""
+Returns a list of all files matching the given file types.
+"""
+_file_types = ['.py']
+
+def find_files(root_directory, file_types=_file_types):
+
+    package_files = []
+
+    for path, directories, files in os.walk( root_directory ):
+        
+        for f in files:
+            
+            path_parts = fullsplit( os.path.join(path, f) )
+
+            path_parts.pop(0)
+
+            this_file = '/'.join(path_parts)
+
+            basename, extension = os.path.splitext( this_file )
+            
+            if extension in file_types:
+
+                package_files.append(this_file)
+
+    return package_files
+
+
+#-------------------------------------------------------------------------------
 NAME = cbi.GetPackageName()
 VERSION = cbi.GetVersion()
-
 AUTHOR = cbi.__author__
 AUTHOR_EMAIL = cbi.__email__
 LICENSE = cbi.__license__
 URL = cbi.__url__
 DOWNLOAD_URL = cbi.__download_url__
-
-DESCRIPTION=""
+DESCRIPTION="The Model Container is a data structure used as an abstration for a model used in a simulation."
+LONG_DESCRIPTION=cbi.__description__
 
 KEYWORDS="neuroscience neurosimulator simulator modeling GENESIS"
 
+# Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
     'Development Status :: 0 - Alpha',
     'Environment :: Console',
@@ -31,31 +112,47 @@ CLASSIFIERS = [
     'Topic :: Research :: Neuroscience',
 ]
 
+PACKAGE_FILES=find_files('heccer')
+
 OPTIONS={
     'sdist': {
         'formats': ['gztar','zip'],
         'force_manifest': True,
         },
-    
     }
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
- 
+PLATFORMS=["Unix", "Lunix", "MacOS X"]
+
+PY_MODULES=['heccer']
+
+
+CMDCLASS = None
+if sys.platform == "darwin": 
+    CMDCLASS = {'install_data': osx_install_data} 
+else: 
+    CMDCLASS = {'install_data': install_data}
+
+
+#-------------------------------------------------------------------------------
+
 setup(
     name=NAME,
     version=VERSION,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
+    cmdclass=CMDCLASS,
+#    data_files=DATA_FILES,
     description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     license=LICENSE,
     keywords=KEYWORDS,
     url=URL,
     packages=['nmc'],
-    long_description=LONG_DESCRIPTION,
+    package_data={'nmc' : PACKAGE_FILES},
+#     package_dir={'' : ''},
     classifiers=CLASSIFIERS,
-    app=APP,
     options=OPTIONS,
-    setup_requires=['py2app'],
+    platforms=PLATFORMS,
+    setup_requires=['g3'],
 )
 
