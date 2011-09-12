d7dae1861cac75fe2d80f89d0a27d497d88b20ce
mandorodriguez <mandorodriguez@gmail.com>
2011-09-12 14:55:34 +0000

    1. Removed a redundant architecture check for the setup.py script.

--- glue/swig/python/setup.py
+++ glue/swig/python/setup.py
@@ -272,11 +272,7 @@ class NMCModule(Extension):
 
         if sys.platform == "darwin":
 
-            arch = autodetect()
-            
-            if arch == 'i386':
-
-                os.environ['ARCHFLAGS'] = self.get_mac_arch_flags()
+            os.environ['ARCHFLAGS'] = self.get_mac_arch_flags()
 
 
         Extension.__init__(self,
