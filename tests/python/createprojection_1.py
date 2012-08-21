9b3720bd5a8fd6e983403279fc7324f9b86ad3f0
mandorodriguez+MacPro <mandorodriguez+MacPro@gmail.com>
2012-08-21 07:28:32 +0000

    1. Updated create projection python test script with dictionary arguments.

--- tests/python/createprojection_1.py
+++ tests/python/createprojection_1.py
@@ -37,11 +37,11 @@ my_model_container.CreateMap('::rscell::/cell', '/RSNet/population', nx, ny, sep
 
 
 config = {'root' : '/RSNet',
-          'projection' : { 'name' : '/RSNet',
+          'projection' : { 'name' : '/RSNet/projection',
                            'source': '../population',
                            'target' : '../population',
                            },
-          'source' : { 'context' : '../population',
+          'source' : { 'context' : '/RSNet/population',
                        { 'include' : { 'type' : 'box',
                                        'coordinates' : [-1e10, -1e10, -1e10, 1e10, 1e10, 1e10],
                                        
@@ -50,48 +50,38 @@ config = {'root' : '/RSNet',
                          
                        },
 
-          'target' : { 'context' : '../population',
+          'target' : { 'context' : '/RSNet/population',
+                       'include' : { 'type' : 'ellipse',
+                                     'coordinates' : [0, 0, 0, sep_x *1.2, sep_y * 1.2, sep_z * 0.5]
+                                     }
+                       'exclude' : 'destination_hole',
+                                   { 'type' : 'box',
+                                     'coordinates' : [sep_x * 0.5, sep_y * 0.5, sep_z * 0.5, sep_x * 0.5, sep_y * 0.5, sep_z * 05],
+                                     },
+                       
+                       
+                           
                        },
 
-          'synapse' : { 'weight' : { 'weight_indicator' : 'weight',
+          'synapse' : { 'pre' : 'spike',
+                        'post' : 'Ex_channel',
+                        'weight' : { 'weight_indicator' : 'weight',
                                      'weight' : syn_weight,
-                                     }
+                                     },
+                        'delay' : { 'delay_indicator' : 'delay',
+                                    'delay_type' : 'fixed',
+                                    'value' : prop_delay
+                                    }
+                        
                         }
+            'probability' : 1.0,
+            'random_seed' : 1212.0
+          
           }
 
 
-source_coords = 
-destination_coords = [0, 0, 0, sep_x *1.2, sep_y * 1.2, sep_z * 0.5]
-
-volume_params = dict(network='/RSNet',
-                     projection='/RSNet/projection',
-                     projection_source='../population',
-                     projection_target='../population',
-                     source='/RSNet/population',
-                     target='/RSNet/population',
-                     pre='spike',
-                     post='Ex_channel',
-                     source_type='box',
-                     destination_type='ellipse',
-                     weight_indicator='weight',
-                     weight=syn_weight,
-                     delay_indicator='delay',
-                     delay_type='fixed',
-                     delay=prop_delay,
-                     velocity_indicator='velocity',
-                     velocity='0.5',
-                     destination_hole_flag='destination_hole',
-                     destination_hole_type='box',
-                     destination_hole_x1=-sep_x * 0.5,
-                     destination_hole_y1=-sep_y * 0.5,
-                     destination_hole_z1=-sep_z * 0.5,
-                     destination_hole_x2=sep_x * 0.5,
-                     destination_hole_y2=sep_y * 0.5,
-                     destination_hole_z2=sep_z * 0.5,
-                     probability='1.0',
-                     random_seed='1212.0')
 
 # use keyword expansion to pass dict as arguments
-my_model_container.VolumeConnect(**volume_params)
+my_model_container.CreateMap(configuration=config)
 
 print "Done!"
